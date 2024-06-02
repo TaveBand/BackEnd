a5105d6e6e4db17806a14aed80192367f252499e
alswndcjswo <alswndcjswo@gmail.com>
2024-06-03 00:49:26 +0900

    first union

--- develop/src/main/java/ys_band/develop/dto/comment/CommentPostDTO.java
+++ develop/src/main/java/ys_band/develop/dto/comment/CommentPostDTO.java
@@ -1,2 +1,16 @@
-package ys_band.develop.dto.comment;public class CommentPostDTO {
+package ys_band.develop.dto.comment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentPostDTO {
+    private Long commentId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private String nickname;
 }
