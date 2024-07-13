8a51054cf3bb0a30cdb6b8e0c3450b128ad9f311
jaehyun <jaehyunj393@gmail.com>
2024-07-13 13:44:42 +0900

    feat: JWT 기반 인증 방식으로 보안 설정 수정
    - UserService를 CustomUserDetailService로 교체
    - JWT 토큰 제공자 및 관련 구성 요소 추가
    - CSRF 보호 비활성화
    - CORS 필터 구성
    - 세션 관리를 무상태(stateless)로 설정
    - JWT 접근 거부 및 인증 진입점에 대한 예외 처리 구성
    - /dailband/login 및 /dailband/register 경로에 대한 접근 허용

--- develop/src/main/java/ys_band/develop/config/SecurityConfig.java
+++ develop/src/main/java/ys_band/develop/config/SecurityConfig.java
@@ -1,72 +1,61 @@
 package ys_band.develop.config;
 
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ys_band.develop.service.UserService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
+@EnableMethodSecurity
+public class SecurityConfig{
+    private final JwtTokenProvider jwtTokenProvider;
 
-    private final UserService userService;
+    private final CustomUserDetailService customUserDetailService;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    // BCryptPasswordEncoder 빈을 생성하여 비밀번호를 암호화하는 데 사용
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider, CustomUserDetailService customUserDetailService, CorsFilter corsFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.customUserDetailService = customUserDetailService;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+
     @Bean
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(CsrfConfigurer::disable)
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionalHandling -> exceptionalHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(configure -> configure.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->auth.requestMatchers("/dailband/login","/dailband/register").permitAll()
+                        .anyRequest().authenticated())
+                .with(new JwtSecurityConfig(jwtTokenProvider, customUserDetailService), customizer->{})
+                .build();
     }
 
-    // 정적 리소스에 대한 요청은 시큐리티 필터를 거치지 않도록 설정
-    //Bean
-    //public WebSecurityCustomizer webSecurityCustomizer() {
-    //    return (web) -> web.ignoring()
-    //            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    //}
-
-    // 시큐리티 필터 체인을 구성하는 메서드
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // CSRF 보호 기능을 비활성화
-                .csrf(csrf -> csrf.disable())
-                // 요청에 대한 권한 설정
-                .authorizeHttpRequests(requests -> requests
-                        // 루트, 로그인, 회원가입 경로는 인증 없이 접근 가
-                        // 그 외의 모든 요청은 인증 필요
-                        .anyRequest().permitAll()
-                )
-                // 폼 로그인 설정
-                .formLogin(form -> form
-                        // 로그인 페이지 설정
-                        .loginPage("/login")
-                        // 로그인 성공 시 리다이렉트할 기본 경로 설정
-                        .defaultSuccessUrl("/home", true)
-                        // 로그인 페이지 접근을 모든 사용자에게 허용
-                        .permitAll()
-                )
-                // 로그아웃 설정
-                .logout(logout -> logout
-                        // 로그아웃 접근을 모든 사용자에게 허용
-                        .permitAll());
-
-        // 설정한 시큐리티 필터 체인을 반환
-        return http.build();
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt Encoder 사용
+        return  new BCryptPasswordEncoder();
     }
 
-    // AuthenticationManagerBuilder를 통해 사용자 인증 서비스와 패스워드 인코더를 설정
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // UserService를 사용하여 사용자 인증을 처리하고,
-        // BCryptPasswordEncoder를 사용하여 비밀번호를 암호화함
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
-    }
+
+
+
 }
