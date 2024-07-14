f582476eef3ad8bebe58d1a0ec4edc08566afdd6
jaehyun <jaehyunj393@gmail.com>
2024-07-14 19:02:11 +0900

    feat: 로그인 로직 구현
    -JWT 인증 방식을 사용해 LoginController 구현
    -UserRepository에서 User 엔티티를 조회할 때 authorities 컬렉션을 함께 로드하여 성능 최적화
    -JWT 설정 추가

--- develop/src/main/java/ys_band/develop/repository/UserRepository.java
+++ develop/src/main/java/ys_band/develop/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ys_band.develop.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ys_band.develop.domain.User;
@@ -7,7 +8,12 @@ import ys_band.develop.domain.User;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository  extends JpaRepository<User,Long> {
+    @EntityGraph(attributePaths = "authorities")
     Optional<User> findByUsername(String username);
+
+    //Optional<User> findByNickname(String nickname);
+
+
+
 }
