cf5c60a286f4e6ea929d5e647255252431d157be
jaehyun <jaehyunj393@gmail.com>
2024-07-14 21:10:40 +0900

    feat: 마이페이지 기능 구현
    -사용자 개인정보 조회/수정
    -관리자 권한으로 사용자 정보 조회
    -사용자가 작성한 게시물과 예약상황 조회

---
+++ develop/src/main/java/ys_band/develop/dto/PostDTO.java
@@ -0,0 +1,47 @@
+package ys_band.develop.dto;
+
+
+import lombok.*;
+import ys_band.develop.domain.BaseTime;
+import ys_band.develop.domain.Post;
+
+import java.util.List;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class PostDTO extends BaseTime {
+
+    private Long postId;
+
+
+    @NonNull
+    private String title;
+    @NonNull
+    private String content;
+
+    private String fileUrl;
+    private List<Comment> comments;
+
+    public static PostDTO fromEntity(Post post){
+        PostDTO postDTO = new PostDTO();
+        postDTO.setPostId(post.getPostId());
+        postDTO.setTitle(post.getTitle());
+        postDTO.setContent(post.getContent());
+
+        if (post.getFile() != null) {
+            postDTO.setFileUrl(post.getFile().getFile_url());
+        } else {
+            postDTO.setFileUrl(null);
+        }
+
+        return postDTO;
+    }
+
+
+}
+
+
