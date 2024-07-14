ea2e8bbd43a8c41c4616dac3f81ed71f29c0e568
jaehyun <jaehyunj393@gmail.com>
2024-07-14 23:02:59 +0900

    fix: 사용자 세션을 복수선택 가능으로 수정
    -세션을 단일 문자열에서 다중 세션 ID 리스트로 변경

--- develop/src/main/java/ys_band/develop/dto/user/UserPostDto.java
+++ develop/src/main/java/ys_band/develop/dto/user/UserPostDto.java
@@ -1,15 +1,22 @@
 package ys_band.develop.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPostDto {
-    private String schoolId;
     private String password;
     private String username;
     private String nickname;
     private String email;
-    private String session;
+    @JsonProperty("sessions")
+    private List<Long> sessionIds;
 }
